e1d714979df7231229392a7de45591fe0ef5b0b6
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2023-10-07 03:03:33 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,177 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.desktop2Parent}>
+        <div className={styles.desktop2}>
+          <div className={styles.aSuperEarth}>A SUPER EARTH ODYSSEY</div>
+          <img className={styles.desktop2Child} alt="" src="/vector-1.svg" />
+          <img className={styles.desktop2Child} alt="" src="/vector-2.svg" />
+          <img className={styles.desktop2Child} alt="" src="/vector-3.svg" />
+          <img className={styles.vectorIcon} alt="" src="/vector-4.svg" />
+          <img className={styles.groupIcon} alt="" src="/group-7.svg" />
+          <div className={styles.title}>
+            <b className={styles.kepler22b}>Kepler 22b</b>
+            <b className={styles.solarSystemExploration}>
+              Solar System Exploration
+            </b>
+          </div>
+          <img className={styles.desktop2Child1} alt="" src="/group-3.svg" />
+          <img className={styles.lightIcon} alt="" src="/light@2x.png" />
+          <div className={styles.textBlockParent}>
+            <div className={styles.textBlock}>
+              <div className={styles.mass}>MASS</div>
+              <div className={styles.earths}>9.1 Earths</div>
+            </div>
+            <div className={styles.textBlock1}>
+              <div className={styles.mass}>PLANET TYPE</div>
+              <div className={styles.earths}>Super Earth</div>
+            </div>
+            <div className={styles.textBlock2}>
+              <div className={styles.mass}>ORBITAL RADIUS</div>
+              <div className={styles.au}>
+                <p className={styles.welcomeToKepler22b}>0.812 AU</p>
+              </div>
+            </div>
+            <div className={styles.textBlock3}>
+              <div className={styles.mass}>DISCOVERY DATE</div>
+              <div className={styles.earths}>2011</div>
+            </div>
+            <div className={styles.textBlock4}>
+              <div className={styles.mass}>PLANET RADIUS</div>
+              <div className={styles.earths}>2.1 x Earth</div>
+            </div>
+            <div className={styles.textBlock5}>
+              <div className={styles.mass}>ECCENTRICITY</div>
+              <div className={styles.earths}>{`< 0.72`}</div>
+            </div>
+          </div>
+          <div className={styles.header}>
+            <div className={styles.r}>
+              <img className={styles.menuIcon} alt="" src="/menu.svg" />
+            </div>
+            <div className={styles.solarTitle}>
+              <p className={styles.welcomeToKepler22b}>
+                Welcome to Kepler-22b!
+              </p>
+            </div>
+            <div className={styles.solarTitle}>
+              <p className={styles.welcomeToKepler22b}>
+                Welcome to Kepler-22b!
+              </p>
+            </div>
+            <div className={styles.solarTitle2}>Home</div>
+            <div className={styles.solarTitle3}>Kepler-22b</div>
+            <div className={styles.solarTitle4}>Features</div>
+            <div className={styles.exploringBeyondOur}>
+              Exploring Beyond Our Cosmic Horizon
+            </div>
+          </div>
+          <img className={styles.image7Icon} alt="" src="/image-7@2x.png" />
+          <div className={styles.rectangleDiv} />
+          <img className={styles.desktop2Child2} alt="" src="/group-22.svg" />
+          <img className={styles.desktop2Child3} alt="" src="/group-22.svg" />
+          <div className={styles.lineDiv} />
+          <div className={styles.desktop2Child4} />
+          <img className={styles.lineIcon} alt="" src="/line-2.svg" />
+          <img className={styles.desktop2Child5} alt="" src="/line-4.svg" />
+          <img className={styles.desktop2Child6} alt="" src="/line-5.svg" />
+          <img className={styles.rectangleIcon} alt="" src="/rectangle-6.svg" />
+          <img
+            className={styles.desktop2Child7}
+            alt=""
+            src="/rectangle-17.svg"
+          />
+          <div className={styles.inTheCosmic}>
+            In the cosmic ballet of distant worlds, one exoplanet often steals
+            the spotlight: Kepler-22b.This distant world, shrouded in the
+            mysteries of the universe, dances through the cosmos, inviting us to
+            ponder its secrets. With its super-Earth stature and a tantalizing
+            position in the habitable zone, Kepler-22b invites us to dream of
+            alien landscapes and the potential for life beyond our own pale blue
+            dot.
+          </div>
+          <div className={styles.aCosmicMarvelContainer}>
+            <p className={styles.welcomeToKepler22b}>
+              A cosmic marvel in Cygnus, 600 light-years from Earth.
+              Coordinates: 19h 16m 52s RA, +47° 53' 3" Dec. An enigmatic world,
+              revealed through advanced telescopes.
+            </p>
+          </div>
+          <img className={styles.desktop2Child8} alt="" src="/group-22.svg" />
+          <div className={styles.kepler22bHarborsAn}>
+            Kepler-22b, Harbors an atmosphere shrouded in mystery. Scientists
+            speculate about its composition, whether it's predominantly rocky,
+            gaseous, or perhaps even liquid. This celestial enigma challenges
+            our understanding of exoplanetary atmospheres, leaving us eager to
+            unveil its secrets.
+          </div>
+          <div className={styles.desktop2Child9} />
+          <img className={styles.desktop2Child10} alt="" src="/vector-5.svg" />
+          <img className={styles.desktop2Child11} alt="" src="/vector-6.svg" />
+          <img
+            className={styles.desktop2Child12}
+            alt=""
+            src="/rectangle-18.svg"
+          />
+          <b className={styles.exploringASuperEarth}>
+            Exploring a Super-Earth Beyond Our Imagination
+          </b>
+          <div className={styles.aCosmicGemInTheCygnusSkiWrapper}>
+            <b className={styles.aCosmicGem}>
+              A Cosmic Gem in the Cygnus Skies
+            </b>
+          </div>
+          <b className={styles.atmosphereUnveilingCelestia}>
+            Atmosphere: Unveiling Celestial Secrets
+          </b>
+          <img
+            className={styles.desktop2Child13}
+            alt=""
+            src="/rectangle-181.svg"
+          />
+          <div className={styles.vectorParent}>
+            <img className={styles.groupChild} alt="" src="/rectangle-11.svg" />
+            <b className={styles.wantToTravel}>Want to travel there?</b>
+            <div className={styles.groupItem} />
+            <div className={styles.groupInner} />
+            <div className={styles.groupChild1} />
+            <div className={styles.groupChild2} />
+            <div className={styles.travelSpeedParent}>
+              <div className={styles.travelSpeed}>Travel speed</div>
+              <div className={styles.div2}>60</div>
+              <div className={styles.milesPerHour}>Miles per hour</div>
+            </div>
+            <div className={styles.travelTimeParent}>
+              <div className={styles.travelSpeed}>Travel Time</div>
+              <div className={styles.div3}>7</div>
+              <div className={styles.billionYears}>Billion years</div>
+            </div>
+            <img className={styles.image11Icon} alt="" src="/image-11@2x.png" />
+            <img className={styles.image8Icon} alt="" src="/image-8@2x.png" />
+            <img className={styles.image10Icon} alt="" src="/image-10@2x.png" />
+            <img className={styles.image12Icon} alt="" src="/image-12@2x.png" />
+            <div className={styles.bulletTrain}>Bullet Train</div>
+            <div className={styles.car}>Car</div>
+            <div className={styles.jet}>Jet</div>
+            <div className={styles.lightSpeed}>
+              <p className={styles.welcomeToKepler22b}>Light Speed</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.groupParent}>
+          <img className={styles.groupChild3} alt="" src="/group-27.svg" />
+          <img className={styles.groupChild4} alt="" src="/group-18.svg" />
+          <div className={styles.teamUfo}> TEAM UFO</div>
+          <div className={styles.unbelievablyFunnyObservers}>
+            Unbelievably Funny Observers
+          </div>
+        </div>
+      </div>
+      <img className={styles.homeChild} alt="" src="/rectangle-171.svg" />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Kepler22b.js
@@ -0,0 +1,142 @@
+import styles from "./Kepler22b.module.css";
+
+const Kepler22b = () => {
+  return (
+    <div className={styles.kepler22b}>
+      <div className={styles.secondLandingPage}>
+        <div className={styles.desktop2}>
+          <img className={styles.desktop2Child} alt="" src="/vector-11.svg" />
+          <img className={styles.desktop2Child} alt="" src="/vector-21.svg" />
+          <img className={styles.desktop2Child} alt="" src="/vector-31.svg" />
+          <img className={styles.vectorIcon} alt="" src="/vector-41.svg" />
+          <div className={styles.kepler22b1}>Kepler-22b</div>
+          <img className={styles.groupIcon} alt="" src="/group-7.svg" />
+          <img className={styles.lightIcon} alt="" src="/light1@2x.png" />
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="/rectangle-62.svg"
+          />
+          <img
+            className={styles.desktop2Child1}
+            alt=""
+            src="/rectangle-5.svg"
+          />
+          <div className={styles.image7Parent}>
+            <img className={styles.image7Icon} alt="" src="/image-71@2x.png" />
+            <div className={styles.image5Parent}>
+              <img className={styles.image5Icon} alt="" src="/image-5@2x.png" />
+              <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+              <div className={styles.areYouReadyToEmbarkOnTheParent}>
+                <div className={styles.areYouReady}>
+                  Are you ready to embark on the journey of a lifetime? Explore
+                  Kepler-22b and redefine what it means to call a new world
+                  "home."
+                </div>
+                <div className={styles.kepler22bYourOdysseyContainer}>
+                  <p className={styles.blankLine}>&nbsp;</p>
+                  <p className={styles.blankLine}>
+                    Kepler-22b: Your Odyssey Awaits
+                  </p>
+                </div>
+                <div className={styles.kepler22bIsntJustContainer}>
+                  <p className={styles.blankLine}>&nbsp;</p>
+                  <p className={styles.blankLine}>
+                    Kepler-22b isn't just another planet; it's a new chapter in
+                    the story of human exploration. Join us as we step into the
+                    future, where dreams of inhabiting distant worlds become a
+                    reality.
+                  </p>
+                </div>
+                <div className={styles.yourNewHome}>Your New Home</div>
+                <div className={styles.kepler22bIsYour}>
+                  Kepler-22b is your canvas for adventure. Embark on thrilling
+                  journeys, explore the unknown, and embrace the awe-inspiring
+                  beauty of a planet like no other.
+                </div>
+                <div className={styles.adventureBeckons}>Adventure Beckons</div>
+                <div className={styles.kepler22bBeckonsScientists}>
+                  Kepler-22b beckons scientists and explorers alike. Conduct
+                  groundbreaking research, study exotic life forms, and unveil
+                  the mysteries of this alien world.
+                </div>
+                <div className={styles.aScientificWonderland}>
+                  A Scientific Wonderland
+                </div>
+                <div className={styles.cuttingEdgeTransportationSy}>
+                  Cutting-edge transportation systems let you roam freely,
+                  venturing into uncharted territories, where every step is a
+                  testament to human curiosity and resilience.
+                </div>
+                <div className={styles.journeyBeyond}>Journey Beyond</div>
+                <div className={styles.lifeHereRequiresContainer}>
+                  <p className={styles.blankLine}>&nbsp;</p>
+                  <p className={styles.blankLine}>
+                    Life here requires innovation. Futuristic habitats and
+                    sustainable agriculture ensure your c
+                    <span className={styles.o}>o</span>mfort and well-being.
+                    Adapt to the gravitational differences and unique atmosphere
+                    that make Kepler-22b unlike any place on Earth.
+                  </p>
+                </div>
+                <div className={styles.adaptingToThe}>
+                  Adapting to the Unknown
+                </div>
+                <div className={styles.kepler22bOffersBreathtaking}>
+                  Kepler-22b offers breathtaking landscapes, from lush forests
+                  to crystal-clear oceans. With technology bridging the gap, you
+                  can traverse its varied terrains, uncovering its secrets.
+                </div>
+                <div className={styles.anAlienLandscape}>
+                  An Alien Landscape
+                </div>
+                <div className={styles.welcomeToKepler22b}>
+                  Welcome to Kepler-22b, a distant exoplanet nestled within the
+                  habitable zone of its star, Kepler-22. Imagine a world where
+                  adventure meets exploration, where humanity's ingenuity
+                  thrives in an alien environment.
+                </div>
+                <div className={styles.discoverKepler22bA}>
+                  Discover Kepler-22b: A New World Awaits
+                </div>
+                <div className={styles.groupChild} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.discoverTheWonders}>
+            {" "}
+            Discover the Wonders Beyond Our Blue Horizon
+          </div>
+          <div className={styles.vectorParent}>
+            <img className={styles.groupItem} alt="" src="/rectangle-7.svg" />
+            <img className={styles.groupInner} alt="" src="/group-182.svg" />
+            <div className={styles.teamUfo}> TEAM UFO</div>
+            <div className={styles.unbelievablyFunnyObservers}>
+              Unbelievably Funny Observers
+            </div>
+          </div>
+          <div className={styles.creatingWorldsBeyond}>
+            Creating worlds beyond our own
+          </div>
+          <div className={styles.frameDiv} />
+        </div>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.r}>
+          <img className={styles.menuIcon} alt="" src="/menu1.svg" />
+        </div>
+        <div className={styles.solarTitle}>
+          <p className={styles.blankLine}>Welcome to Kepler-22b!</p>
+        </div>
+        <div className={styles.solarTitle}>
+          <p className={styles.blankLine}>Welcome to Kepler-22b!</p>
+        </div>
+        <div className={styles.solarTitle2}>Home</div>
+        <div className={styles.solarTitle3}>Kepler-22b</div>
+        <div className={styles.solarTitle4}>Features</div>
+      </div>
+    </div>
+  );
+};
+
+export default Kepler22b;
